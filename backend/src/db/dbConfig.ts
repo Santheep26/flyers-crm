959a0ab0cd9890e61419bf9b84d4cf04561a5b26
santheep26 <santheepstellon26@gmail.com>
2023-06-19 14:01:11 +0530

    Fixed the code as per comments

--- backend/src/db/dbConfig.ts
+++ backend/src/db/dbConfig.ts
@@ -5,8 +5,6 @@ const mongooConnect = async () => {
     mongoose.connect(process.env.MONGO_URI as string);
     console.log(
       "Database Connected Successfully"
-      // connect.connection.host,
-      // connect.connection.name
     );
   } catch (err) {
     console.log("Error Message is: ", err);
